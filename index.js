61ce1e1940bfd5e1dbd2b401d8dba0a6f11cdefd
rafizul896 <rafizulislam896@gmail.com>
2024-04-28 00:34:32 +0600

    get and post add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -25,8 +25,30 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const paintingAndDrawing = client.db("paintingAndDrawing").collection("craftItemsDB");
+
+        app.get('/craftItmes', async (req, res) => {
+            const result = await paintingAndDrawing.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/craftItmes/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await paintingAndDrawing.findOne(query);
+            res.send(result)
+        })
+
+        app.post('/craftItmes', async (req, res) => {
+            const newCraftItmes = req.body;
+            console.log(newCraftItmes);
+            const result = await paintingAndDrawing.insertOne(newCraftItmes);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
